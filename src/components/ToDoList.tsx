b100c36bf78c3e8738685e389dd724391e65703b
SugarSyrup <tlfvm04@naver.com>
2022-12-21 23:01:54 +0900

    추가: Recoil Selector

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,20 +1,26 @@
-import { useRecoilValue } from "recoil";
-import { toDoState } from "../atoms";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { categoryState, toDoSelector } from "../atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const todos = useRecoilValue(toDoSelector);
+  const [category, setCategory] = useRecoilState(categoryState);
+  const onInput = (event:React.FormEvent<HTMLSelectElement>) => {
+    const { currentTarget : {value}} = event;
+    setCategory(value);
+  }
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
+      <select value={category} onInput={onInput}>
+        <option value="TO_DO">To Do</option>
+        <option value="DOING">Doing</option>
+        <option value="DONE">Done</option>
+      </select>
       <CreateToDo />
-      <ul>
-        {toDos.map((toDo) => (
-          <ToDo key={toDo.id} {...toDo} />
-        ))}
-      </ul>
+      {todos?.map(toDo => <ToDo key={toDo.id} {...toDo}/>)}
     </div>
   )
 }
